4b6727ff3bae259b3fa6e54f2f2936cffcd394c4
jieunlee0000 <jieunlee.johnny@gmail.com>
2025-12-26 17:00:18 +0900

    수정 내용에 대한 설명 작성

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,2 @@
 // 메인 페이지 스크립트
 // 스와이프
-var swiper = new Swiper('.mySwiper', {
-    pagination: {
-        el: '.swiper-pagination',
-    },
-});
--- assets/js/plugin.js
+++ assets/js/plugin.js
@@ -3,3 +3,28 @@
 let visualSwiper = null;
 
 // gsap 스크립트 등
+
+// main visual swiper
+
+var swiper = new Swiper('.main_visual_image', {
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    loop: true,
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+    },
+});
+
+// business swiper
+var swiper = new Swiper('.projects_visual', {
+    slidesPerView: 4,
+    // centeredSlides: true,
+    spaceBetween: 30,
+    grabCursor: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
